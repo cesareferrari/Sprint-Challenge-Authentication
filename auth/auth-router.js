38888144187f12b85622010c37b37450dc706093
Cesare Ferrari <cesare@maryandferrari.com>
2019-08-31 13:09:17 -0400

    Add new user

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
+const User = require('../user/user-model.js');
 
-router.post('/register', (req, res) => {
-  // implement registration
+router.post('/register', async (req, res) => {
+  const user = req.body;
+
+  try {
+    const newUser = await User.add(user);
+    console.log("newUser", newUser);
+    if (newUser) {
+      res.status(201).json(newUser);
+    } else {
+      res.status(400).json({message: "Required credentials"});
+    }
+  } catch (err) {
+    res.status(500).json({message: "Error creating the user"})
+  }
 });
 
 router.post('/login', (req, res) => {
